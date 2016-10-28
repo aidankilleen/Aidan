29d432143cce163425215d8d9257888a45224164
Aidan Killeen <aidan.killeen@gmail.com>
2016-10-28 09:17:06 +0100

    added missing months

--- src/main/java/ie/rccourse/ClassWithTests.java
+++ src/main/java/ie/rccourse/ClassWithTests.java
@@ -20,6 +20,16 @@ public class ClassWithTests {
 		months.add("January");
 		months.add("February");
 		months.add("March");
+		months.add("April");
+		months.add("May");
+		months.add("June");
+		months.add("July");
+		months.add("August");
+		months.add("September");
+		months.add("October");
+		months.add("November");
+		months.add("December");
+		
 	}
 
 }
